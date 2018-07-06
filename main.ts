8e38c49bd3f629f3496c6e7c0f72748e88bdfddb
Ids <idsmesales@gmail.com>
2018-07-06 21:53:46 +0200

    RXJS55>using fromEvent

--- main.ts
+++ main.ts
@@ -1,14 +1,19 @@
 // Add you sand-box code here and lets play with RXJS.
-import {Observable} from "rxjs";
-
-alert("oke webpack & typescript working time to start coding with the RXJS lib");
-
-let numbers = [1, 2, 3, 4, 5];
-let subscription = Observable.from(numbers);
-
-subscription.subscribe(
-    (dataItem) => console.log(`${dataItem}`),
-    (error)=> console.error(`Error detected:>`,error),
-    ()=>console.info("Observer completed"));
 
 
+
+// Normal way of adding a listener.
+import {Observable} from "rxjs/Observable";
+import "rxjs/add/observable/fromEvent";
+import "rxjs/add/operator/scan";
+
+const my_button_normal = document.querySelector('#my-button-normal');
+const my_button_observable = document.querySelector('#my-button-observable');
+
+let count = 1;
+my_button_normal.addEventListener('click', (result) => console.log(`Clicker de click. From normal addEventListener:>count:>${count++}`));
+
+// The RxJs way
+Observable.fromEvent(my_button_observable, 'click')
+    .scan(counter  => +counter + 1, 0)
+    .subscribe({ next: result => console.log(`Clicker the Click fromEvents Observable>counter:>`,result)});
