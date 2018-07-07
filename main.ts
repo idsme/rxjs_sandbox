1264cecd84a9ce11611584cb172a99fbcc83ece9
Ids <idsmesales@gmail.com>
2018-07-07 23:11:11 +0200

    RXJS55>Playing with concatAll operator, delay while inter observables complete.

--- main.ts
+++ main.ts
@@ -1,14 +1,27 @@
 // Add you sand-box code here and lets play with RXJS.
-import {Observable} from "rxjs";
+import {interval} from "rxjs/observable/interval";
 
-alert("oke webpack & typescript working time to start coding with the RXJS lib");
-
-let numbers = [1, 2, 3, 4, 5];
-let subscription = Observable.from(numbers);
-
-subscription.subscribe(
-    (dataItem) => console.log(`${dataItem}`),
-    (error)=> console.error(`Error detected:>`,error),
-    ()=>console.info("Observer completed"));
+import {of} from "rxjs/observable/of";
+import "rxjs/add/operator/take";
+import {concatAll} from "rxjs/operators";
 
 
+const source1 = interval(2000).take(5);
+const source2 = interval(2000).take(3);
+const source3 = interval(2000).take(1);
+
+const source = of(source1, source2, source3);
+
+const example = source.pipe(concatAll());
+
+example.subscribe((data) => console.log('output>', data));
+
+// output> 0
+// main.ts:17 output> 1
+// main.ts:17 output> 2
+// main.ts:17 output> 3
+// main.ts:17 output> 4
+// main.ts:17 output> 0
+// main.ts:17 output> 1
+// main.ts:17 output> 2
+// main.ts:17 output> 0
