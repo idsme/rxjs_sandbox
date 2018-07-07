492e0b845235063f5a9b2dd696f9337d26c4edf0
Ids <idsmesales@gmail.com>
2018-07-07 23:48:25 +0200

    RXJS55>Playing the first and last operator

--- main.ts
+++ main.ts
@@ -1,14 +1,15 @@
 // Add you sand-box code here and lets play with RXJS.
 import {Observable} from "rxjs";
+import {first, last} from "rxjs/operators";
 
 alert("oke webpack & typescript working time to start coding with the RXJS lib");
 
 let numbers = [1, 2, 3, 4, 5];
-let subscription = Observable.from(numbers);
+let source = Observable.from(numbers);
 
-subscription.subscribe(
-    (dataItem) => console.log(`${dataItem}`),
-    (error)=> console.error(`Error detected:>`,error),
-    ()=>console.info("Observer completed"));
+// Get the first item only 1
+source.pipe(first()).subscribe((data)=> console.log('data.first item only>', data));
 
 
+// Get the last item only 5
+source.pipe(last()).subscribe((data)=> console.log('data.last item only>', data));
